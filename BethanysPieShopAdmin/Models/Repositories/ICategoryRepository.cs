86f8bcc610ddb7433f77c48329df414437cf16b0
nafizrifat <imtiaz_rifat@yahoo.com>
2026-05-06 22:11:50 -0500

    Add project files.

---
+++ BethanysPieShopAdmin/Models/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace BethanysPieShopAdmin.Models.Repositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories();
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category?> GetCategoryByIdAsync(int id);
+
+    }
+}
